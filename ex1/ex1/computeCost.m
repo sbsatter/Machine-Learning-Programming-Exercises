5375f9f2cabfa826c09dcbeb38930c2da572871a
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2017-12-03 16:42:24 +0600

    remove comment

--- ex1/ex1/computeCost.m
+++ ex1/ex1/computeCost.m
@@ -14,7 +14,7 @@ J = 0;
 %               You should set J to the cost.
 pred = X * theta;
 squaredErrs = (pred - y).^2;
-J = 1 / (2 * m)  *  sum(squaredErrs)
+J = 1 / (2 * m)  *  sum(squaredErrs);
 
 
 % =========================================================================
