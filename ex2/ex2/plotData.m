dbb1c134f0cf7a2c47cb9e39e7fbd77c2ca6e07c
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2017-12-06 16:54:39 +0600

    upto predict

--- ex2/ex2/plotData.m
+++ ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -11,15 +11,11 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
-
-
-
-
-
-
-
-
-
+pos = find(y==1);
+neg = find(y==0);
+plot(X(pos, 1), X(pos, 2), 'k+', 'lineWidth', 2, 'MarkerSize', 7);
+hold on;
+plot(X(neg, 1), X(neg, 2), 'ko', 'markerfacecolor', 'y', 'markerSize', 7);
 % =========================================================================
 
 
