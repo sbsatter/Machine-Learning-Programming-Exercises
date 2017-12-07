d311d00c81bf5a89152c94590ca0bcc1a593ea94
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2017-12-07 18:10:25 +0600

    using keyword end

--- ex2/ex2/costFunctionReg.m
+++ ex2/ex2/costFunctionReg.m
@@ -17,9 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 [J, grad] = costFunction(theta, X, y);
-J = J + (lambda / (2*m) * sum(theta(2:size(theta)).^2));
+J = J + (lambda / (2*m) * sum(theta(2:end).^2));
 
-grad = grad + [0;lambda / m * (theta(2:size(theta)))]
+grad = grad + [0;lambda / m * (theta(2:end))]
 
 
 
